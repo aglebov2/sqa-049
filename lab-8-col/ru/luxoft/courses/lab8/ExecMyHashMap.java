ca07c971071c5ba57e6f588cc2b08caa43a46a1b
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-14 23:21:11 +0600

    Lab8 - refactored a bit. - ok!

--- lab-8-col/ru/luxoft/courses/lab8/ExecMyHashMap.java
+++ lab-8-col/ru/luxoft/courses/lab8/ExecMyHashMap.java
@@ -1,12 +1,9 @@
 package ru.luxoft.courses.lab8;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class ExecMyHashMap {
     public static void main(String[] args) {
 
-        MyHashMap <String, Integer> map = new MyHashMap<>();
+        MyHashMap<String, Integer> map = new MyHashMap<>();
         System.out.println("This is The program ExecMyHashMap");
 
         map.put("Ball", 2);
@@ -29,23 +26,23 @@ public class ExecMyHashMap {
         System.out.println("Dome == Dome: " + map.get("Dome").equals(map.get("Dome")));
 
         System.out.println("0:1:200");
-        MyHashMap <Integer, Integer> mapInt = new MyHashMap<>();
-        for (int j = 0; j <202; j++){
+        MyHashMap<Integer, Integer> mapInt = new MyHashMap<>();
+        for (int j = 0; j < 202; j++) {
             mapInt.put(j, j);
         }
         System.out.println("3_size: " + mapInt.size());
-        for (int a = 0; a < 202; a++){
+        for (int a = 0; a < 202; a++) {
             System.out.println("value for key " + a + ": " + mapInt.get(a));
         }
         System.out.println("4_size: " + mapInt.size());
 
         System.out.println("0:16:200");
         mapInt = new MyHashMap<>();
-        for (int j = 0; j <202; j += 16){
+        for (int j = 0; j < 202; j += 16) {
             mapInt.put(j, j);
         }
         System.out.println("5_size: " + mapInt.size());
-        for (int a = 0; a < 202; a += 16){
+        for (int a = 0; a < 202; a += 16) {
             System.out.println("value for key " + a + ": " + mapInt.get(a));
         }
         System.out.println("6_size: " + mapInt.size());
